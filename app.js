4695a139a7a615f4eead996ee6287c11bfa078f3
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-26 21:43:34 +0530

    Initialize server setup with Express, MongoDB connection, and basic configuration files. Added .gitignore, app.js, database.js, and package.json for dependencies and scripts.

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const connectDB = require("./config/database");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch((err) => {
+  console.log(err);
+});
+
