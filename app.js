864781a1084770968d05b04e32c760e1a15d1622
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-30 16:12:00 +0530

    Update CORS configuration in app.js to allow requests from the production URL

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://quizappjoseph.netlify.app/",
     credentials: true,
 }));
 
